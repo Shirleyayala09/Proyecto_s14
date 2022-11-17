612ebbfb4357aa9de65dcc81c964c6ebcdcaac55
Shirley Ayala <sayalac16_1@unc.edu.pe>
2022-11-17 17:00:19 -0500

    S16

--- S16/Ejercicio.js
+++ S16/Ejercicio.js
@@ -1,27 +1,23 @@
 let x =+prompt("Ingrese el primer número" )
 let y=+prompt("Ingrese el segundo número")
 
-function multipli(x,y){
- 
+function multipli(a,b){
+return a*b
 };
-var multipli= x * y;
  
-function expon(x,y){
+function expon(a,b){
+    return Math.pow(a,b)
     };
-var expon= Math.pow(x,y);
 
-function suma(x,y){
-    
+function suma(a,b){
+    return a +b
 };
-var suma = x + y
-
-function resta(x,y){
+function resta(a,b){
+return a-b
 };
 
-var resta= x - y;
-
-alert(`Al multiplicar: ${x} x ${y} nos da ${multipli}.
-Al alevar  ${x} a la ${y} nos da ${expon}.
-Al sumar  ${x} + ${y} nos da ${suma}.
-Al restar ${x} - ${y} nos da ${resta}.`);
+alert(`Al multiplicar: ${x} x ${y} nos da ${multipli(x,y)}.
+Al alevar  ${x} a la ${y} nos da ${expon(x,y)}.
+Al sumar  ${x} + ${y} nos da ${suma(x,y)}.
+Al restar ${x} - ${y} nos da ${resta(x,y)}.`);
 
